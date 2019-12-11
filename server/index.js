1def013f0b8aa7b2ba5bd3d601c6144c2b31ede3
jonathanhkim <jhkim792@gmail.com>
2019-12-10 16:17:13 -0800

    various changes to help with configuration

--- server/index.js
+++ server/index.js
@@ -2,7 +2,46 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const port = 3000
+const cors = require('cors')
 
+app.use(cors())
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.static('http://localhost:3001/Public'))
+
+app.get('/graph/*', (req, res) => {
+  const url = `http://localhost:3001/graph/getStocks?q=${req.query.id}`
+  res.redirect(url)
+})
+
+app.get('/tradestock/*', (req, res) => {
+  const url = `http://localhost:3002/tradestock/${req.params[0]}`
+  res.redirect(url)
+})
+
+app.get('/about/*', (req ,res) => {
+  const url = `http://localhost:3003/about/${req.params[0]}`
+  res.redirect(url)
+})
+
+app.get('/news/getData', (req ,res) => {
+  const url = `http://localhost:3004/news/getData?id=${req.query.id}`
+  res.redirect(url)
+})
+
+app.post('/news/addViews', (req, res) => {
+  const url = `http://localhost:3004/news/addViews?id=${req.query.id}`
+  res.redirect(url)
+})
+
+app.get('/ratings/*', (req, res) => {
+  const url = `http://localhost:3005/ratings/${req.params[0]}`
+  res.redirect(url)
+})
+
+app.get('/earnings/*', (req, res) => {
+  const url = `http://localhost:3006/earnings/getData?id=${req.query.id}`
+  res.redirect(url)
+})
+app.get('/')
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
 module.exports = {
   entry: {
-    vendor: ["styled-components"]
+    vendor: ["styled-components"],
   },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  optimization: {
+    splitChunks: {
       name: "vendor",
       minChunks: Infinity
-    })
-  ],
+    }
+  },
   resolve: {
     alias: {
-      "styled-components": path.resolve(RobinHoodProxy, "node_modules", "styled-components")
+      "styled-components": path.join(__dirname, "node_modules", "styled-components"),
     }
-  }
+  },
+  externals: {
+    "styled-components": {
+      commonjs: "styled-components",
+      commonjs2: "styled-components",
+      amd: "styled-components",
+    },
+  },
 }
